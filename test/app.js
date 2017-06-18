35275908af6adfecf17311b96ad014acfd72084e
alincode <alincode@gmail.com>
2017-06-19 00:40:10 +0800

    update

--- test/app.js
+++ test/app.js
@@ -1,7 +1,21 @@
-var calc = require('../lib/app.js');
+var app = require('../lib/app.js');
 var assert = require('assert');
 
-describe('calc', () => {
-
-
+describe('計算機', () => {
+  it('加', () => {
+    var result = app.addition(1, 2);
+    assert.equal(result, 3)
+  });
+  it('減', () => {
+    var result = app.subtraction(2, 1);
+    assert.equal(result, 1);
+  });
+  it('乘', () => {
+    var result = app.multiplication(2, 3);
+    assert.equal(result, 6);
+  });
+  it('除', () => {
+    var result = app.division(6, 3);
+    assert.equal(result, 2)
+  });
 });
